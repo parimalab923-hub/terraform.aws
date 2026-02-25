ab58dd0732cc53545f2da6b61d1f8dd7a6598986
parimalab923-hub <parimalab923@gmail.com>
2026-02-25 10:54:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "myec2" {
-  ami           = "ami-0c1fe732b5494dc14"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-058510d1467a8b83c"
-  key_name = "parimala_keypair"
+  ami           = var.ami
+  instance_type = var.instancetype
+  subnet_id = var.subnetid
+  key_name = var.keyname
   tags = {
-    Name = "myec2"
+    Name = var.ec2_name
   }
 }
