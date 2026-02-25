ffd59992acc0cb76c63512a35e5b011d56d32183
parimalab923-hub <parimalab923@gmail.com>
2026-02-25 17:07:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,113 @@
+resource "aws_vpc" "myvpc" {
+
+    cidr_block = var.vpc_cidr
+    instance_tenancy = "default"
+     tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_internet_gateway" "myigw" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = var.igw_name
+  }
+}
+resource "aws_route_table" "myroute" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myigw.id
+  }
+
+  tags = {
+    Name = var.route_name
+  }
+}
+
+resource "aws_subnet" "publicsubnet" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = var.subnet_cidr
+
+  tags = {
+    Name = var.subnet_name
+  }
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.3.0/24"
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.mysubnet.id
+  route_table_id = aws_route_table.myroute.id
+}
+
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+}
+
 resource "aws_instance" "myec2" {
   ami           = var.ami
   instance_type = var.instancetype
   subnet_id = var.subnetid
+  security_groups = [aws_security_group.ec2_sg.id]
   key_name = var.keyname
   tags = {
     Name = var.ec2_name
   }
+}
+
+resource "aws_lb" "alb" {
+  subnets         = [aws_subnet.public1.id, aws_subnet.public2.id]
+  security_groups = [aws_security_group.alb_sg.id]
+}
+resource "aws_lb_target_group" "tg" {
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.myvpc.id
+}
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+resource "aws_lb_target_group_attachment" "attach" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.app.id
+  port             = 80
+}
+resource "aws_db_instance" "rds" {
+  allocated_storage = 20
+  engine            = "mysql"
+  instance_class    = "db.t2.micro"
+  username          = "admin"
+  password          = "password123"
+  skip_final_snapshot = true
 }
