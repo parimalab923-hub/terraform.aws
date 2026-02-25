a57fab91aa0d9ebba9ab8e6df8759d08cc909981
parimalab923-hub <parimalab923@gmail.com>
2026-02-25 11:20:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 6.32"
     }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
